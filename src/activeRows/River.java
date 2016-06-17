67cf02d41a5d1419fd7c8b84c47fb49f7cefa10c
Lorenz Kock <lkock@vater-gruppe.de>
2016-06-17 08:14:34 +0200

    no message

--- src/activeRows/River.java
+++ src/activeRows/River.java
@@ -7,13 +7,13 @@ import utils.Settings;
 public class River extends ActiveRow {
 	
 	public River(int direction, int speed, int row, Playground playground) {
+		this.playground = playground;
+		this.settings = playground.getGameFrame().getMainFrame().getSettings();
 		this.setDirection(direction);
 		this.setSpeed(speed);
 		this.setRow(row);
 		this.addMovableObject(new MovableObject(settings.TRUNK,120,this));
 		this.start();
-		this.playground = playground;
-		this.settings = playground.getGameFrame().getMainFrame().getSettings();
 	}
 	
 	@Override
